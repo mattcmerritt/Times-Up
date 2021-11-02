0bd7929c6b02d9d364c692d14e1ce973942ddb1c
mattcmerritt <mattcmerritt@gmail.com>
2021-11-02 06:20:16 -0400

    built main menu and simple settings screens

---
+++ Assets/Scripts/UI/SceneScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//added to load scenes
+using UnityEngine.SceneManagement;
+
+public class SceneScript : MonoBehaviour
+{
+    //a new variable that you can change in the inspector
+    public string nextLevel;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //a public method that will run when you click the button
+    public void ButtonToChangeLevel()
+    {
+
+        //loads the scene written in the inspector
+        SceneManager.LoadScene(nextLevel);
+
+    }
+}
