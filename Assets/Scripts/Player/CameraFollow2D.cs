6c0058d69d86af459e7f8f1c23923d2b7e4d7601
mattcmerritt <mattcmerritt@gmail.com>
2021-11-16 07:07:59 -0500

    added walk and jump animations

---
+++ Assets/Scripts/Player/CameraFollow2D.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow2D : MonoBehaviour
+{
+
+    private Transform playerPosition;
+    public float camOffsetPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerPosition = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void LateUpdate()
+    {
+        Vector3 camCurrent = transform.position;
+        camCurrent.x = playerPosition.position.x;
+        camCurrent.y = playerPosition.position.y;
+        camCurrent.x += camOffsetPos;
+        transform.position = camCurrent;
+    }
+}
